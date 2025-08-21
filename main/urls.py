6c56be0473abd9c3dd1e43ca3db4bebb2741029e
meshack34 <meshackkimutai34@gmail.com>
2025-08-21 18:32:07 +0300

    css

--- main/urls.py
+++ main/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path("register/", views.register, name="register"),
     path("login/", views.user_login, name="login"),
     path("logout/", views.user_logout, name="logout"),
+    
+    path("services/", views.services_view, name="services"),
+    path("solutions/", views.solutions_view, name="solutions"),
+    path("about/", views.about_view, name="about"),
+    path("contact/", views.contact_view, name="contact"),
 ]
 # crb/settings.py
--- main/views.py
+++ main/views.py
@@ -48,3 +48,14 @@ def home_view(request):
         'testimonials': testimonials
     })
 
+def services_view(request):
+    return render(request, "main/services.html")
+
+def solutions_view(request):
+    return render(request, "main/solutions.html")
+
+def about_view(request):
+    return render(request, "main/about.html")
+
+def contact_view(request):
+    return render(request, "main/contact.html")
