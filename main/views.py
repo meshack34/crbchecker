e1562ee28853b45c6371e8d7461a8b51012a3576
meshackkimutai34 <meshackkimutai34@gmail.com>
2025-08-24 18:32:02 +0300

    updates

--- main/views.py
+++ main/views.py
@@ -5,8 +5,8 @@ from django.contrib import messages
 from .forms import RegisterForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib import messages
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import ReportPurposeForm
 
 
 def register(request):
@@ -54,10 +54,6 @@ def home_view(request):
     })
 
 
-
-from django.shortcuts import render, redirect
-from django.contrib.auth.decorators import login_required
-from .forms import ReportPurposeForm
 
 @login_required
 def select_report_purpose(request):
